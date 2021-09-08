69eb4b98669e7c90d8c2651d711c8864396355fe
jaeout <jaeout2@gmail.com>
2021-09-08 17:26:56 +0900

    GET/POST 오류 수정하기

--- src/main/java/com/board/workspace/BoardController.java
+++ src/main/java/com/board/workspace/BoardController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.board.workspace.BoardService;
 
@@ -19,6 +20,11 @@ public class BoardController {
 	@RequestMapping(value="/listAll", method=RequestMethod.GET) //주소 호출 명시.호출하려는 주소와 REST방식 설정(GET)
 	public void listAll(Model model)throws Exception{ //메소드 인자값은 model인터페이스(jsp를 전달하는 심부름꾼)
 		model.addAttribute("list",service.listAll()); //jsp에 심부름할 내역(서비스 호출) 
-		
+	}
+	
+	@RequestMapping(value="/regist", method=RequestMethod.POST) //post방식으로 내용 전송
+	public String registPOST(BoardVO board, RedirectAttributes rttr) throws Exception{ //인자값으로 redirect사용 
+		service.regist(board); //글작성 서비스 호출
+		return "redirect:/listAll"; //작성이 완료된 후 목록 페이지로 리턴
 	}
 }
