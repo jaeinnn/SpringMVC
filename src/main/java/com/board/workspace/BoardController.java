642d25b27d610a2c1b610e5e7f64b6b658ddf0f8
jaeout <jaeout2@gmail.com>
2021-09-14 17:38:39 +0900

    read 컨트롤러

--- src/main/java/com/board/workspace/BoardController.java
+++ src/main/java/com/board/workspace/BoardController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.board.workspace.BoardService;
@@ -18,8 +19,8 @@ public class BoardController {
 	private BoardService service; //Service 호출을 위한 객체 생성 
 	
 	@RequestMapping(value="/listAll", method=RequestMethod.GET) //주소 호출 명시.호출하려는 주소와 REST방식 설정(GET)
-	public void listAll(Model model)throws Exception{ //메소드 인자값은 model인터페이스(jsp를 전달하는 심부름꾼)
-		model.addAttribute("list",service.listAll()); //jsp에 심부름할 내역(서비스 호출) 
+	public void listAll(Model model) throws Exception { //메소드 인자값은 model인터페이스(jsp를 전달하는 심부름꾼)
+		model.addAttribute("list", service.listAll()); //jsp에 심부름할 내역(서비스 호출) 
 	}
 	
 	@RequestMapping(value="/regist", method=RequestMethod.GET)
@@ -32,4 +33,11 @@ public class BoardController {
 		service.regist(board); //글작성 서비스 호출
 		return "redirect:/listAll"; //작성이 완료된 후 목록 페이지로 리턴
 	}
+	
+	@RequestMapping(value="/read", method=RequestMethod.GET)//GET방식으로 페이지 호출 
+	public void read(@RequestParam("bno")int bno, Model model) throws Exception{
+		model.addAttribute(service.read(bno)); //read서비스 호출
+	}
+	
+	
 }
