5807be4fcaaa909691150861373f453cfeec8ce2
jaeout <jaeout2@gmail.com>
2021-09-17 10:02:04 +0900

    글 삭제 기능 추가

--- src/main/java/com/board/workspace/BoardController.java
+++ src/main/java/com/board/workspace/BoardController.java
@@ -11,34 +11,53 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.board.workspace.BoardService;
 import com.board.workspace.BoardVO;
 
-@Controller //컨트롤러임을 명시
-@RequestMapping(value= "/") //주소 패턴
+@Controller // 컨트롤러임을 명시
+@RequestMapping(value = "/") // 주소 패턴
 public class BoardController {
-	
-	@Inject //주입임을 명시
-	private BoardService service; //Service 호출을 위한 객체 생성 
-	
-	@RequestMapping(value="/listAll", method=RequestMethod.GET) //주소 호출 명시.호출하려는 주소와 REST방식 설정(GET)
-	public void listAll(Model model) throws Exception { //메소드 인자값은 model인터페이스(jsp를 전달하는 심부름꾼)
-		model.addAttribute("list", service.listAll()); //jsp에 심부름할 내역(서비스 호출) 
+
+	@Inject // 주입임을 명시
+	private BoardService service; // Service 호출을 위한 객체 생성
+
+	@RequestMapping(value = "/listAll", method = RequestMethod.GET) // 주소 호출 명시.호출하려는 주소와 REST방식 설정(GET)
+	public void listAll(Model model) throws Exception { // 메소드 인자값은 model인터페이스(jsp를 전달하는 심부름꾼)
+		model.addAttribute("list", service.listAll()); // jsp에 심부름할 내역(서비스 호출)
+	}
+
+	@RequestMapping(value = "/regist", method = RequestMethod.GET)
+	public void registerGET(BoardVO board, Model model) throws Exception {
+
+	}
+
+	@RequestMapping(value = "/regist", method = RequestMethod.POST) // post방식으로 내용 전송
+	public String registPOST(BoardVO board, RedirectAttributes rttr) throws Exception { // 인자값으로 redirect사용
+		service.regist(board); // 글작성 서비스 호출
+		return "redirect:/listAll"; // 작성이 완료된 후 목록 페이지로 리턴
+	}
+
+	@RequestMapping(value = "/read", method = RequestMethod.GET) // GET방식으로 페이지 호출
+	public void read(@RequestParam("bno") int bno, Model model) throws Exception {
+		// 인자값은 파라미터 값으로 기본키인 글번호를 기준으로 Model을 사용하여 불러옴
+		model.addAttribute(service.read(bno)); // read서비스 호출
+	}
+
+	@RequestMapping(value = "/modify", method = RequestMethod.GET) // GET 방식으로 페이지 호출
+	public void modifyGET(int bno, Model model) throws Exception {
+		model.addAttribute(service.read(bno)); // 수정을 위한 글읽기 서비스 호출
+	}
+
+	@RequestMapping(value = "/modify", method = RequestMethod.POST) // POST방식으로 데이터 전송
+
+	public String modifyPOST(BoardVO board, RedirectAttributes rttr) throws Exception {
+		service.modify(board); // 글수정 서비스 호출
+		return "redirect:/listAll"; // 수정이 완료된 후, 목록페이지로 리턴
+
 	}
 	
-	@RequestMapping(value="/regist", method=RequestMethod.GET)
-	public void registerGET(BoardVO board,Model model)throws Exception{
+	
+	@RequestMapping(value="/remove", method=RequestMethod.POST) //POST방식으로 데이터 전송
+	public String removePOST(@RequestParam("bno") int bno,RedirectAttributes rttr)throws Exception{
 		
+		service.remove(bno); //글삭제 서비스 호출
+		return "redirect:/listAll"; //삭제가 완료된 후, 목록페이지로 리턴
 	}
-	
-	@RequestMapping(value="/regist", method=RequestMethod.POST) //post방식으로 내용 전송
-	public String registPOST(BoardVO board, RedirectAttributes rttr) throws Exception{ //인자값으로 redirect사용 
-		service.regist(board); //글작성 서비스 호출
-		return "redirect:/listAll"; //작성이 완료된 후 목록 페이지로 리턴
-	}
-	
-	@RequestMapping(value="/read", method=RequestMethod.GET)//GET방식으로 페이지 호출 
-	public void read(@RequestParam("bno")int bno, Model model) throws Exception{
-		//인자값은 파라미터 값으로 기본키인 글번호를 기준으로 Model을 사용하여 불러옴
-		model.addAttribute(service.read(bno)); //read서비스 호출
-	}
-	
-	
 }
